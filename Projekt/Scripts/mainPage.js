49423de1cf4e799ee475fb807032a143ef452e27
Codynator <113552830+Codynator@users.noreply.github.com>
2023-05-14 22:08:44 +0200

    Update 8

--- Projekt/Scripts/mainPage.js
+++ Projekt/Scripts/mainPage.js
@@ -1,5 +1,7 @@
-import data from "./data/test.json" assert { type: "json"};;
-console.log(data);
-
 import premiers from "./data/premiers.json" assert { type: "json"};
-console.log(premiers)
+import movies from "./data/movies.json" assert { type: "json"};
+const btn1 = document.getElementById("btn-1");
+const btn2 = document.getElementById("btn-2");
+const btn3 = document.getElementById("btn-3");
+const moviesContainer = document.getElementById("movies-container");
+
